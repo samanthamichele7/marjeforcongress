0448d0fa2e8d3f3c15eb3d5c562c7fc6bc8a9899
Samantha Geitz <samanthamichele7@gmail.com>
2019-10-07 16:46:57 -0500

    Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,61 @@
-<footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+<footer class="content-info bg-mfcDarkBlue py-8">
+  <div class="container mx-auto flex flex-wrap">
+    <div class="w-full md:w-1/2 px-4 lg:px-16 mb-4">
+      <div class="text-xl text-white font-header italic font-bold ml-3">Stay Connected</div>
+      <!-- Begin Mailchimp Signup Form -->
+      <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+      <div id="mc_embed_signup">
+        <form action="https://marjeforcongress.us20.list-manage.com/subscribe/post?u=7914cf387522b9567f7cb4c6a&amp;id=dbb1d92f4d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7914cf387522b9567f7cb4c6a_dbb1d92f4d" tabindex="-1" value=""></div>
+            <div class="clear">
+              <input type="submit" value="Subscribe" name="subscribe" class="bg-mfcGreen hover:bg-green-600 cursor-pointer text-white rounded px-4 py-1">
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <!--End mc_embed_signup-->
+    </div>
+
+    <div class="w-full md:w-1/2 px-8 md:px-16">
+      <a 
+        href="https://fb.me/marjeforcongress"
+        class="flex items-center text-white hover:text-mfcLightBlue mb-4"
+        target="__blank"
+      >
+        <i class="fab fa-facebook-f border border-white hover:border-mfcLightBlue h-8 w-8 flex items-center justify-center rounded-full mr-4"></i>
+        fb.me/marjeforcongress
+      </a>
+
+      <a 
+        href="https://twitter.com/marje2020"
+        class="flex items-center text-white hover:text-mfcLightBlue mb-4"
+        target="__blank"
+      >
+        <i class="fab fa-twitter border border-white hover:border-mfcLightBlue h-8 w-8 flex items-center justify-center rounded-full mr-4"></i>
+        @marje2020
+      </a>
+
+      <a 
+        href="tel:218-353-1552"
+        class="flex items-center text-white hover:text-mfcLightBlue mb-4"
+        target="__blank"
+      >
+        <i class="fas fa-phone border border-white hover:border-mfcLightBlue h-8 w-8 flex items-center justify-center rounded-full mr-4"></i>
+        218-353-1552
+      </a>
+
+      <a 
+        href="mailto:info@marjeforcongress.com"
+        class="flex items-center text-white hover:text-mfcLightBlue mb-4"
+        target="__blank"
+      >
+        <i class="far fa-envelope border border-white hover:border-mfcLightBlue h-8 w-8 flex items-center justify-center rounded-full mr-4"></i>
+        info@marjeforcongress.com
+      </a>
+    </div>
   </div>
 </footer>
