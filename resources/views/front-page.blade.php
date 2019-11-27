5d9bb08baf64484cd8f32349936df5f6b61ef121
Samantha Geitz <samanthamichele7@gmail.com>
2019-11-27 15:03:59 -0600

    Announcement and remove Brightest

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -4,11 +4,13 @@
   @include('partials.page-header')
 
   <div class="flex flex-wrap">
-    <div class="w-full md:w-2/3">
-        <div class="bg-mfcDarkBlue h-96 flex items-center justify-center">
+    <div class="w-full md:w-1/2">
+        <div class="bg-mfcDarkBlue h-128 flex items-center justify-center">
             <img src="@asset('images/logo-light.png')" alt="Marje for Congress logo" class="h-36 sm:h-48 lg:h-64" />
         </div>
+    </div>
 
+    <div class="w-full md:w-1/2">
         <div class="slideshow h-128" data-slideshow>
             <img src="@asset('images/homepage/1.jpg')" class="h-96" />
             <img src="@asset('images/homepage/2.jpg')" class="h-96" />
@@ -19,114 +21,33 @@
             <img src="@asset('images/homepage/7.jpg')" class="h-96" />
         </div>
     </div>
-
-    <div class="w-full md:w-1/3 bg-mfcGreen">
-        <div class="mx-auto text-center">
-            <link href="https://brightest-production.s3.amazonaws.com/static/css/embed.css" rel="stylesheet" /> <div id="brightest-embed-form-073fc2d2d92b40198cd77f0154786b5a"></div> <script type="text/javascript"> var brightest_embedded_form_context = { formLink: "https://www.brightest.io/cause/marjeforcongress/embed/712/", target: "brightest-embed-form-073fc2d2d92b40198cd77f0154786b5a" }; </script> <script src="https://brightest-production.s3.amazonaws.com/static/js/embed.js"></script>
-        </div>
-    </div>
   </div>
 
   <div class="container mx-auto my-16 px-8 lg:px-12">
-    <div class="flex flex-wrap border-b border-gray-400 pb-8">
+    <div class="flex flex-wrap pb-8">
         <div class="w-full lg:w-3/5">
-            <div class="text-2xl text-mfcDarkBlue font-header italic font-bold mx-auto">People First</div>
-
             <p>
-                In the course of my lifetime, I’ve observed that American government has shifted to promote and protect 
-                big money, big banks, and big business. It seems to me that policy and politics are driven by 
-                “profit first, people second.”
+                After a great deal of thought, I've come to the difficult decision to suspend my congressional campaign. In recent days, it has become evident that the time and resource demands of a congressional campaign and the requirements of my life as a working parent are not compatible. Though my reasons for running are public, and have not changed, the private needs of my family must take priority for a time. If I must choose between pursuing a seat in Congress or the people I love who depend on me - the obvious choice is people first.
             </p>
 
             <p>
-                I disagree with that direction for America. I believe — as Abraham Lincoln said — in <i>“government of the people, 
-                by the people, for the people.”</i> I’m running because I want a government that puts people first. 
+                Though I feel disappointment at suspending my campaign so soon after it started, I know that this is not the end of my path in political activism. I remain convinced that we need voices that will speak out for the needs of working-class folks, advocate for affordable and accessible health care, and push for rural broadband to expand economic diversity and educational opportunity for all.
             </p>
 
-
-            <p class="ml-4 mt-8">
-                <span class="text-mfcDarkBlue italic font-bold">People First</span> means strong labor laws and unions to protect 
-                all who work, with livable wages that support a family on 40 hours of work.
+            <p>
+                We want to live in a nation where we can discuss challenging problems with kindness, civility and empathy for everyone affected by those problems. We want a nation that abides by the laws of Constitutional democracy. We want a nation where every citizen has a voice that is heard and recognized. The only way to achieve that nation is to continue to speak out when elected officials ignore the law and abuse their power. I will continue to speak out.
             </p>
 
-            <p class="ml-4">
-                <span class="text-mfcDarkBlue italic font-bold">People First</span> means all citizens have access to 
-                affordable health care and prescription medication, and do not face bankruptcy in order to survive.
+            <p>
+                Obviously, the 8th Congressional District has changed greatly in the past 50 years, but I think we can still be inspired by the leadership of Representatives like John Blatnik, James Oberstar and Rick Nolan who fought for the hard-working people of Minnesota and the nation. They weren't always "nice," as our current Congressman is labeled, but they were effective, influential and got the job done. They were scrappy. Their past work was an inspiration for me, and should be for the person who aspires to be our next Representative in Congress.
             </p>
 
-            <p class="ml-4">
-                <span class="text-mfcDarkBlue italic font-bold">People First</span> means we look for ways to expand 
-                economic opportunities through rural broadband initiatives and educational partnerships to bring 
-                world class education to our small towns and communities.
-            </p>
-
-            <p class="ml-4">
-                <span class="text-mfcDarkBlue italic font-bold">People First</span> means we protect our democratic freedoms and every eligible vote counts.
-            </p>
-
-            <p class="text-lg text-mfcLightBlue italic font-bold mt-8 -mr-8">
-                People First — because we deserve a government that works for us.
-            </p>
+            <p class="text-lg text-mfcDarkBlue italic font-bold pt-4">This is not the end of my journey. This is the beginning. And I'm scrappy.</p>
         </div>
 
         <div class="w-full lg:w-2/5 pt-8">
             <img src="@asset('images/homepage/marje.jpg')" alt="Marje photo" class="rounded-full p-4 md:p-8 mx-auto lg:-mr-8 mb-8" />
         </div>
-    </div>
-  </div>
-
-  <div style="background: linear-gradient(180deg, white, rgba(141, 198, 63, 0.2), rgba(141, 198, 63, 0.5));">
-    <div class="container mx-auto pb-16 px-8 lg:px-12">
-        <div class="flex flex-wrap items-center lg:pb-8">
-            <div class="w-full lg:w-1/2 xl:w-1/3 mb-8 lg:mb-0 lg:pr-16">
-                <img src="@asset('images/homepage/cd8.png')" alt="MN CD8" class="h-128 h-auto w-64 md:w-128 mx-auto shadow" />
-            </div>
-
-            <div class="w-full lg:w-1/2 xl:w-2/3 mb-8 lg:mb-0">
-                <p class="text-gray-600 text-xl">
-                    I grew up on Minnesota’s Iron Range, a descendent of Finnish immigrant farmers and miners. I myself 
-                    spent a summer working in the fines crusher at Minntac — that third shift job paid for a year of college. 
-                    Coming from a family with five children, money was always tight, but love was always abundant. 
-                    I know that my dad’s job as a union steelworker and the government assistance we qualified for was one 
-                    of the reasons we had enough during lean years for American steel.   
-                </p>
-            </div>
-        </div>
-        
-        <p class="text-lg text-mfcDarkBlue italic font-bold pt-4">
-            Why I want to serve CD8 in Congress
-        </p>
-
-        <p>
-            My story is the story of all immigrant families, who start with the hope of a better future for 
-            their children and work hard to make it reality.
-        </p>
-
-        <p>
-            I announced my campaign because I can’t stand politics.
-        </p>
-
-        <p>
-            We are living in an era where kindness, civility, concern, and obvious compassion for our neighbors 
-            is absent from the national political conversation. 
-        </p>
-
-        <p>
-            We are living in an era where programs that have direct impact on the well-being of our most at-risk 
-            citizens are losing funding and meaningful change is nearly impossible because politicians deny 
-            that they have control over these policy choices.
-        </p>
-
-        <p>
-            We are living in an era with some of the greatest economic disparity in history because shareholders 
-            are placed at a higher value than the workers who create the products being sold. Economic prosperity 
-            is not easily accessible if you weren’t already born into economic prosperity. 
-        </p>
-
-        <p class="font-bold">
-            I’m running because I can’t stand politics, and if I don’t stand up and use the skills and talents given to 
-            me to try to change that, the future that I will leave to my own children will be worse than the one granted to me. 
-        </p>
     </div>
   </div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,36 +1,10 @@
 <footer class="content-info bottom">
-  <div class="w-full bg-mfcLightBlue flex flex-col items-center justify-center text-center py-8">
-    <div class="text-lg md:text-2xl text-white font-header italic font-bold mx-auto pb-2 px-8">Let's work together to put People First.</div>
-
-    <p class="text-white mb-4 mx-8 md:mx-16">Donate via Act Blue</p>
-
-    <div class="flex justify-between mb-4 border-b border-gray-300 pb-4">
-        <a class="bg-white hover:bg-gray-200 border border-gray-500 text-mfcDarkBlue font-bold mx-1 md:mx-2 py-2 px-2 rounded text-xs sm:text-sm" href="https://secure.actblue.com/donate/marje?amount=5">$5</a>
-        <a class="bg-white hover:bg-gray-200 border border-gray-500 text-mfcDarkBlue font-bold mx-1 md:mx-2 py-2 px-2 rounded text-xs sm:text-sm" href="https://secure.actblue.com/donate/marje?amount=10">$10</a>
-        <a class="bg-white hover:bg-gray-200 border border-gray-500 text-mfcDarkBlue font-bold mx-1 md:mx-2 py-2 px-2 rounded text-xs sm:text-sm" href="https://secure.actblue.com/donate/marje?amount=25">$25</a>
-        <a class="bg-white hover:bg-gray-200 border border-gray-500 text-mfcDarkBlue font-bold mx-1 md:mx-2 py-2 px-2 rounded text-xs sm:text-sm" href="https://secure.actblue.com/donate/marje?amount=50">$50</a>
-        <a class="bg-white hover:bg-gray-200 border border-gray-500 text-mfcDarkBlue font-bold mx-1 md:mx-2 py-2 px-2 rounded text-xs sm:text-sm" href="https://secure.actblue.com/donate/marje?amount=100">$100</a>
-        <a class="bg-white hover:bg-gray-200 border border-gray-500 text-mfcDarkBlue font-bold mx-1 md:mx-2 py-2 px-2 rounded text-xs sm:text-sm" href="https://secure.actblue.com/donate/marje">$Other</a>
-    </div>
-
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-        <input type="hidden" name="cmd" value="_s-xclick" />
-        <input type="hidden" name="hosted_button_id" value="JSLJTC957PTCJ" />
-        <div class="bg-mfcGreen hover:bg-green-600 text-white mx-2 pt-2 pb-3 px-4 rounded cursor-pointer">
-            <span class="absolute mt-1">Donate Via Paypal</span>
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" class="opacity-0 h-6 w-32" />
-        </div>
-        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-    </form>
-  </div>
 
   <div class="bg-mfcDarkBlue">
     <div class="container mx-auto flex flex-wrap">
-      <div class="w-full md:w-1/2 px-4 lg:pl-32 mb-4 text-center md:text-left">
-        <div> <link href="https://brightest-production.s3.amazonaws.com/static/css/embed.css" rel="stylesheet" /> <div id="brightest-embed-form-9462e5e5d4b84410915c8eab1c2c95a2"></div> <script type="text/javascript"> var brightest_embedded_form_context = { formLink: "https://www.brightest.io/cause/marjeforcongress/embed/764/", target: "brightest-embed-form-9462e5e5d4b84410915c8eab1c2c95a2" }; </script> <script src="https://brightest-production.s3.amazonaws.com/static/js/embed.js"></script> </div>
+      <div class="w-full md:w-1/2 px-4 lg:pl-32 mb-4 text-center md:text-left flex items-center">
       
-        <div class="font-bold">
-          <div class="flex flex-wrap">
+        <div class="font-bold flex flex-col w-full">
             <a 
               href="https://twitter.com/marje2020"
               class="flex items-center text-white hover:text-mfcLightBlue mb-4 w-1/2"
@@ -58,7 +32,6 @@
               info@marjeforcongress.com
             </a>
           </div>
-        </div>
       </div>
 
       <div class="w-full md:w-1/2 py-8 text-center">
